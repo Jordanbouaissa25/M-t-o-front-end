772a9e274c3475376eb3d55c86524504baaa7a5c
Jordanbouaissa25 <jordanbouaissa25@mail.com>
2024-09-02 16:20:31 +0200

    Country à rajouter

--- src/Application/Pages/SearchPage.tsx
+++ src/Application/Pages/SearchPage.tsx
@@ -1,33 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FiSunrise, FiSunset } from "react-icons/fi";
 import { FaTwitter, FaInstagram, FaFacebook } from 'react-icons/fa';
 import { FaSearch } from "react-icons/fa";
 import { MdOutlineVisibility } from "react-icons/md";
-import { NavLink } from 'react-router-dom';
 import { Footer } from "../../Components/footer";
 import humidity_icon from '../../assets/humidity.png';
 import clear_icon from '../../assets/clear.png';
 import wind_icon from '../../assets/wind.png';
 import { http } from '../../Infrastructure/Http/Axios.Instance';
-import axios from 'axios';
-
-const API_KEY = '91fbde8f0b5ad7adc0d2262673e3bd6c'; // Remplacez par votre propre clé API
 
 // Définition des types pour les données météo
 interface WeatherData {
-  name: string;
-  main: {
-    temp: number;
-    humidity: number;
-  };
-  sys: {
-    sunrise: number;
-    sunset: number;
-  };
+  city: string;
+
+  temp: number;
+  humidity: number;
+  sunrise: number;
+  sunset: number;
   visibility: number;
-  wind: {
-    speed: number;
-  };
+  wind: number;
 }
 
 export const SearchPage: React.FC = () => {
@@ -35,24 +26,51 @@ export const SearchPage: React.FC = () => {
   const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
   const [error, setError] = useState<string | null>(null);
 
+//   useEffect(() => {
+//   const mockData = {
+//     name: "Paris",
+//     main: {
+//       temp: 15,
+//       humidity: 80
+//     },
+//     sys: {
+//       sunrise: 1600000000,
+//       sunset: 1600040000
+//     },
+//     visibility: 10000,
+//     wind: {
+//       speed: 5
+//     }
+//   };
 
-  const fetchWeatherData = async (city: string) => {
+//   setWeatherData(mockData);
+// }, []);
+
+
+
+   const fetchWeatherData = async (city: string) => {
     try {
-      const response = await axios.post('/weather', { city });
-      console.log(response.data); // Vérifier la réponse du backend
+        console.log(`Recherche des données météo pour la ville: ${city}`);
+        const response = await http.post(`/weather?city=${city}`);
+         console.log('Statut de la réponse :', response.status);
+        console.log('Données météo reçues:', response.data);
 
-      if (response.data && response.data.name) {
-        setWeatherData(response.data);
-        setError(null);
-      } else {
-        setError("Ville non trouvée. Veuillez réessayer.");
-      }
+        console.log('Données météo reçues:', response.data);
+
+        if (response.data && response.data.city) {
+            setWeatherData(response.data);
+            setError(null);
+        } else {
+            setError("Ville non trouvée. Veuillez réessayer.");
+        }
     } catch (err) {
-      console.error("Erreur lors de l'appel au backend :", err);
-      setError("Erreur de communication avec le serveur. Veuillez réessayer.");
-      setWeatherData(null);
+        console.error("Erreur lors de l'appel au backend :", err);
+        setError("Erreur de communication avec le serveur. Veuillez réessayer.");
+        setWeatherData(null);
     }
-  };
+};
+
+console.log(weatherData)
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
@@ -63,25 +81,25 @@ export const SearchPage: React.FC = () => {
     fetchWeatherData(search);
   };
 
-   return (
+  return (
     <div className="flex flex-col min-h-screen justify-between bg-[#2D2C5A] text-white">
-         <header className="flex justify-between items-center p-5 bg-[#2D2C5A]">
-      <div className="flex items-center">
-        <img src="../../public/logo.png" alt="Logo météo" className="h-10" />
-        <p className="ml-4 text-white">Météo</p>
-      </div>
-      <div className="flex space-x-4">
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-          <FaTwitter className="text-white h-6 w-6 hover:text-blue-400" />
-        </a>
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-          <FaInstagram className="text-white h-6 w-6 hover:text-pink-500" />
-        </a>
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-          <FaFacebook className="text-white h-6 w-6 hover:text-blue-600" />
-        </a>
-      </div>
-    </header>
+      <header className="flex justify-between items-center p-5 bg-[#2D2C5A]">
+        <div className="flex items-center">
+          <img src="../../public/logo.png" alt="Logo météo" className="h-10" />
+          <p className="ml-4 text-white">Météo</p>
+        </div>
+        <div className="flex space-x-4">
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+            <FaTwitter className="text-white h-6 w-6 hover:text-blue-400" />
+          </a>
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+            <FaInstagram className="text-white h-6 w-6 hover:text-pink-500" />
+          </a>
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+            <FaFacebook className="text-white h-6 w-6 hover:text-blue-600" />
+          </a>
+        </div>
+      </header>
 
       {/* Barre de recherche */}
       <div className="px-5 mt-1 flex flex-col">
@@ -111,19 +129,19 @@ export const SearchPage: React.FC = () => {
             <img src={clear_icon} alt="Icône du temps clair" className="" />
 
             {/* Température */}
-            <p className="text-4xl font-bold">{weatherData.main.temp}°C</p>
+            <p className="text-4xl font-bold">{weatherData.temp}°C</p>
 
             {/* Ville */}
-            <p className="text-xl">{weatherData.name}</p>
+            <p className="text-xl">{weatherData.city}</p>
 
             {/* Heure du lever de soleil */}
             <div className="flex items-center space-x-2">
               <FiSunrise className="w-6 h-6" />
-              <p className="text-lg">{new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
+              <p className="text-lg">{new Date(weatherData.sunrise * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
             </div>
             <div className="flex items-center space-x-2">
               <FiSunset className='w-6 h-6' />
-              <p className='text-lg'>{new Date(weatherData.sys.sunset * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
+              <p className='text-lg'>{new Date(weatherData.sunset * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</p>
             </div>
             <div className='flex items-center space-x-2'>
               <MdOutlineVisibility className='w-6 h-6' />
@@ -133,7 +151,7 @@ export const SearchPage: React.FC = () => {
             <div className="flex items-center space-x-2">
               <img src={humidity_icon} alt="IconHumidity" className="w-6 h-6" />
               <div className="flex flex-col items-start">
-                <p className="text-lg">{weatherData.main.humidity}%</p>
+                <p className="text-lg">{weatherData.humidity}%</p>
               </div>
             </div>
 
@@ -141,7 +159,7 @@ export const SearchPage: React.FC = () => {
             <div className="flex items-center space-x-2">
               <img src={wind_icon} alt="IconWind" className="w-6 h-6" />
               <div className="flex flex-col items-start">
-                <p className="text-lg">{weatherData.wind.speed} km/h</p>
+                <p className="text-lg">{weatherData.wind} km/h</p>
               </div>
             </div>
           </>
--- src/Infrastructure/Http/Axios.Instance.ts
+++ src/Infrastructure/Http/Axios.Instance.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const token :string|null = localStorage.getItem("token")
-
 export const http = axios.create({
   baseURL: 'http://localhost:3001', // Assurez-vous que cela correspond à l'URL de votre backend
   headers: {
